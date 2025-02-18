8835e87c3546d1fc16a7364e7bd1c8d58fc3bb16
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-18 13:32:13 -0500

    add tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
 import shortlyLogo from './assets/shortly-logo.svg'
 
 function App() {
 
   return (
     <>
-      <img src={shortlyLogo} alt="" />
+      <img src={shortlyLogo} alt="Shortly logo"/>
     </>
   )
 }
