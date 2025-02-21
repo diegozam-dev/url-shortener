765b17818113e74cd82c1d056d6bb892697f54b9
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-21 16:49:08 -0500

    refactor

--- frontend/src/components/layouts/HomeLayout.tsx
+++ frontend/src/components/layouts/HomeLayout.tsx
@@ -4,7 +4,11 @@ import ShortenUrl from '../../features/shortenUrl/ShortenUrl';
 const HomeLayout = () => {
   return (
     <div className="mx-auto px-6 max-w-[1032px]">
-      <img src={shortlyLogo} alt="Shortly logo" className="my-10 mx-auto" />
+      <img
+        src={shortlyLogo}
+        alt="Shortly logo"
+        className="my-10 mx-auto w-40"
+      />
       <section id="shorten-url" className="my-10">
         <h1 className="text-(--heading-primary) text-center mb-6">
           Shorten Url
