49bf09671f155b3bd945fc7674b6da7b9a6f44e9
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-24 19:47:37 -0500

    add footer, add container for all content and update styles

--- frontend/src/components/layouts/HomeLayout.tsx
+++ frontend/src/components/layouts/HomeLayout.tsx
@@ -2,34 +2,55 @@ import shortlyLogo from '../../assets/shortly-logo.svg';
 import ShortenUrl from '../../features/shortenUrl/ShortenUrl';
 import waves from '../../assets/waves.png';
 import ShortenedUrlHistory from '../../features/shortenedUrlHistory/ShortenedUrlHistory';
+import Footer from './Footer';
 
 const HomeLayout = () => {
   return (
-    <main className="mx-auto px-6 max-w-[1032px] overflow-visible">
-      <img
-        src={shortlyLogo}
-        alt="Shortly logo"
-        className="my-10 mx-auto w-40 z-50"
-      />
-      <section id="shorten-url" className="my-10 min-h-96 z-50">
-        <ShortenUrl />
-      </section>
-      <div className="absolute left-0 top-[550px] min-[370px]:top-[500px] sm:top-[450px] w-full h-80 overflow-hidden flex justify-center -z-10">
-        <div className="w-full h-full">
-          <img
-            src={waves}
-            alt="Waves"
-            className="w-full h-full object-cover object-top"
-          />
+    <div>
+      <main className="mx-auto px-6 max-w-[1032px] overflow-visible">
+        <img
+          src={shortlyLogo}
+          alt="Shortly logo"
+          className="py-10 mx-auto w-40 z-50"
+        />
+        <section
+          id="shorten-url"
+          className="py-10 h-[500px] z-50 min-[370px]:h-[416px]"
+        >
+          <ShortenUrl />
+        </section>
+        <div className="absolute left-0 top-[600px] min-[370px]:top-[520px] sm:top-[450px] w-full h-80 overflow-hidden flex justify-center -z-10">
+          <div className="w-full h-full">
+            <img
+              src={waves}
+              alt="Waves"
+              className="w-full h-full object-cover object-top"
+            />
+          </div>
         </div>
-      </div>
-      <section
-        id="shortened-url-history"
-        className="pt-36 sm:pt-12 pb-10 z-50 min-[370px]:pt-24"
-      >
-        <ShortenedUrlHistory />
-      </section>
-    </main>
+        <section
+          id="shortened-url-history"
+          className="pt-32 pb-10 z-50 min-[370px]:pt-34 sm:pt-16"
+        >
+          <ShortenedUrlHistory />
+        </section>
+        <section id="about" className="py-16 z-50 flex flex-col items-center">
+          <h2 className="text-(--heading-primary) text-center mb-6">
+            About Shortly
+          </h2>
+          <p className="text-(--text-primary) text-xl text-center max-w-[500px]">
+            <strong>
+              Shortly is a simple and efficient tool for shortening any link
+              without losing its original functionality.
+            </strong>{' '}
+            With just one click, it transforms long URLs into compact, shareable
+            versions. Simply paste your link into the field, click "Shorten",
+            and instantly get a short link ready to share with your friends.
+          </p>
+        </section>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
