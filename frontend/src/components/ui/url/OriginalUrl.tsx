2473d7c25a4bfc3d993ac6d43935b64932b99641
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-26 16:07:10 -0500

    update styles and import

--- frontend/src/components/ui/url/OriginalUrl.tsx
+++ frontend/src/components/ui/url/OriginalUrl.tsx
@@ -1,7 +1,7 @@
-import { UrlProps } from '../../../types';
+import { UrlProps } from '../../../types/component';
 
 const OriginalUrl = ({ url }: UrlProps) => {
-  return <p className="text-base text-(--text-primary)">{url}</p>;
+  return <p className="text-base text-(--text-primary) line-clamp-1">{url}</p>;
 };
 
 export default OriginalUrl;
