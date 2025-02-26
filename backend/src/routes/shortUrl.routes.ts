7a5675a3201327c7d12c3a078c9ce242640860e2
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-26 16:10:36 -0500

    comment cache

--- backend/src/routes/shortUrl.routes.ts
+++ backend/src/routes/shortUrl.routes.ts
@@ -3,13 +3,13 @@ import ShortUrlController from '../controllers/shortUrl.controller';
 import scanUrl from '../middlewares/scanUrl.middleware';
 import validateUrl from '../middlewares/validateUrl.middleware';
 import rateLimiter from '../middlewares/rateLimiter.middleware';
-import cache from '../utils/cacheConfig';
+// import cache from '../utils/cacheConfig';
 
 const shortUrlController = new ShortUrlController();
 const shortUrlRouter = Router();
 
 // Middlewares
-shortUrlRouter.use(cache());
+// shortUrlRouter.use(cache());
 
 shortUrlRouter.post(
   '/encode',
