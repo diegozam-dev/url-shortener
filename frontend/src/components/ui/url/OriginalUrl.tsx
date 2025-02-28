ac2ef8c5a045dd4ac7bed737784ef9f671502a69
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-28 15:09:41 -0500

    add styles for ellipsis in text

--- frontend/src/components/ui/url/OriginalUrl.tsx
+++ frontend/src/components/ui/url/OriginalUrl.tsx
@@ -1,7 +1,11 @@
 import { UrlProps } from '../../../types/component';
 
 const OriginalUrl = ({ url }: UrlProps) => {
-  return <p className="text-base text-(--text-primary) line-clamp-1">{url}</p>;
+  return (
+    <p className="text-base text-(--text-primary) whitespace-nowrap overflow-hidden text-ellipsis">
+      {url}
+    </p>
+  );
 };
 
 export default OriginalUrl;
--- frontend/src/components/ui/url/ShortUrl.tsx
+++ frontend/src/components/ui/url/ShortUrl.tsx
@@ -5,7 +5,7 @@ const ShortUrl = ({ url }: UrlProps) => {
     <a
       href={url}
       target="_blank"
-      className="text-base line-clamp-1 text-(--short-url-text) font-semibold cursor-pointer transition-all hover:text-(--short-url-text-hover)"
+      className="text-base whitespace-nowrap overflow-hidden text-ellipsis text-(--short-url-text) font-semibold cursor-pointer transition-all hover:text-(--short-url-text-hover)"
     >
       {url}
     </a>
