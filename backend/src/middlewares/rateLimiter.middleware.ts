1cb0341db3192c77d3336d3ab7cf1a41fa108b23
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-28 15:07:42 -0500

    update the maximum limit to 100

--- backend/src/middlewares/rateLimiter.middleware.ts
+++ backend/src/middlewares/rateLimiter.middleware.ts
@@ -3,7 +3,7 @@ import ErrorEnum from '../errors/errorEnum';
 
 const rateLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000,
-  max: 10,
+  max: 100,
   message: {
     response: ErrorEnum.TooManyRequests,
     message: 'You have exceeded your 10 requests per day limit.'
