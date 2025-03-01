563aa20c06f07f2dc66276279abb8de6148da780
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-01 15:41:17 -0500

    change type message to informative

--- frontend/src/utils/copyTextToClipboard.ts
+++ frontend/src/utils/copyTextToClipboard.ts
@@ -2,7 +2,7 @@ import { toast } from 'sonner';
 
 const copyTextToClipboard = async (text: string) => {
   await navigator.clipboard.writeText(text);
-  toast.success(`Copied: ${text}`);
+  toast.info(`Copied: ${text}`);
 };
 
 export default copyTextToClipboard;
