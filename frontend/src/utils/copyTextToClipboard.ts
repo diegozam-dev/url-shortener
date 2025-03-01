d2e809f97be7acf1f2e8a33f3fe49bf1e80749d8
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-01 14:32:58 -0500

    add copied text to toast

--- frontend/src/utils/copyTextToClipboard.ts
+++ frontend/src/utils/copyTextToClipboard.ts
@@ -2,7 +2,7 @@ import { toast } from 'sonner';
 
 const copyTextToClipboard = async (text: string) => {
   await navigator.clipboard.writeText(text);
-  toast.success('Copied');
+  toast.success(`Copied: ${text}`);
 };
 
 export default copyTextToClipboard;
