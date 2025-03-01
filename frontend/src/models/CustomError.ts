ac1e93570073b924b0ad2e892a1e9268bec6cd8a
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-01 14:33:30 -0500

    add CustomError class

---
+++ frontend/src/models/CustomError.ts
@@ -0,0 +1,18 @@
+import { ErrorCode } from '../types/error';
+
+class CustomError extends Error {
+  private readonly _code: ErrorCode;
+
+  constructor(code: ErrorCode, message?: string) {
+    super(message);
+    this._code = code;
+
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  get code(): string {
+    return this._code;
+  }
+}
+
+export default CustomError;
