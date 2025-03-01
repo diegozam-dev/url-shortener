82daa8f9c51783f9e3d23e50740ef5c9805d22d6
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-01 14:33:39 -0500

    add handlerError

---
+++ frontend/src/services/handleErrors.ts
@@ -0,0 +1,25 @@
+import { toast } from 'sonner';
+import CustomError from '../models/CustomError';
+import { ErrorCode } from '../types/error';
+
+export const handleErrors = (err: CustomError) => {
+  let errorMessage: string;
+
+  if (err.code === ErrorCode.UnknownError) {
+    errorMessage = 'Something happened! The operation could not be completed.';
+  } else if (err.code === ErrorCode.AuthRequired) {
+    errorMessage =
+      'The operation could not be completed. Please try again later.';
+  } else if (err.code === ErrorCode.InvalidUrl) {
+    errorMessage = 'The url is invalid, check the url and try again.';
+  } else if (err.code === ErrorCode.MaliciousUrl) {
+    errorMessage =
+      'The url entered could be malicious, check the url and try again.';
+  } else if (err.code === ErrorCode.TooManyRequests) {
+    errorMessage = 'You have exceeded the limit of 100 requests per day.';
+  } else {
+    errorMessage = 'Something happened! The operation could not be completed.';
+  }
+
+  toast.error(errorMessage);
+};
