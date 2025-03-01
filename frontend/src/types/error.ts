d3fe923f29036e00d298e241eb981b94062fbe07
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-01 14:33:19 -0500

    add error codes

---
+++ frontend/src/types/error.ts
@@ -0,0 +1,9 @@
+export enum ErrorCode {
+  MaliciousUrl = 'ERR_MALICIOUS_URL',
+  NotFound = 'ERR_NOT_FOUND',
+  InvalidUrl = 'ERR_INVALID_URL',
+  UrlNotExists = 'ERR_URL_NOT_EXISTS',
+  TooManyRequests = 'ERR_TOO_MANY_REQUESTS',
+  AuthRequired = 'ERR_AUTH_REQUIRED',
+  UnknownError = 'ERR_UNKNOWN_ERROR'
+}
