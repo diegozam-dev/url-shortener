1d4d4af86c3f301396fee25184a9ff5fb0bffabb
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-03 15:01:53 -0500

    remove try catch

--- frontend/src/services/urlShortenerService.ts
+++ frontend/src/services/urlShortenerService.ts
@@ -1,29 +1,17 @@
 import axios, { AxiosResponse } from 'axios';
-import { handleErrors } from './handleErrors';
-import CustomError from '../models/CustomError';
-import { ErrorCode } from '../types/index';
 
 export const createShortUrl = async (url: string) => {
-  try {
-    const { data }: AxiosResponse = await axios.post(
-      import.meta.env.VITE_ENCODE_URL_API,
-      {
-        url
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
+  const { data }: AxiosResponse = await axios.post(
+    import.meta.env.VITE_ENCODE_URL_API,
+    {
+      url
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json'
       }
-    );
-
-    return data;
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      const errorCode = error.response?.data?.response as ErrorCode;
-      handleErrors(new CustomError(errorCode || ErrorCode.UnknownError));
-    } else {
-      handleErrors(new CustomError(ErrorCode.UnknownError));
     }
-  }
+  );
+
+  return data;
 };
