7ae7a631af245882e1b31b48be7430b756822dcb
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-03 14:45:43 -0500

    change name from errorEnum to errorCodes and add some error codes

--- backend/src/errors/errorCodes.ts
+++ backend/src/errors/errorCodes.ts
@@ -1,11 +1,13 @@
-enum ErrorEnum {
+enum ErrorCode {
   MaliciousUrl = 'ERR_MALICIOUS_URL',
   NotFound = 'ERR_NOT_FOUND',
   InvalidUrl = 'ERR_INVALID_URL',
   UrlNotExists = 'ERR_URL_NOT_EXISTS',
   TooManyRequests = 'ERR_TOO_MANY_REQUESTS',
   AuthRequired = 'ERR_AUTH_REQUIRED',
+  WrongCredentialsError = 'ERR_WRONG_CREDENTIALS',
+  ErrorNetwork = 'ERR_NETWORK',
   UnknownError = 'ERR_UNKNOWN_ERROR'
 }
 
-export default ErrorEnum;
+export default ErrorCode;
