1d704c7bf9e51b785a0d1819210473188f1b44a7
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-03 14:48:10 -0500

    add validations to axios errors and virusTotal errors

--- backend/src/middlewares/scanUrl.middleware.ts
+++ backend/src/middlewares/scanUrl.middleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import VirusTotalService from '../services/virusTotal.service';
-import ErrorEnum from '../errors/errorEnum';
+import ErrorCode from '../errors/errorCodes';
+import axios from 'axios';
+import CustomError from '../errors/customError.error';
 
 const scanUrl = async (req: Request, res: Response, next: NextFunction) => {
   const { url } = req.body;
@@ -16,16 +18,31 @@ const scanUrl = async (req: Request, res: Response, next: NextFunction) => {
 
     if (malicious > 0 && suspicious > 0) {
       res.status(400).json({
-        response: ErrorEnum.MaliciousUrl,
+        response: ErrorCode.MaliciousUrl,
         message: 'The url sent could be malicious.'
       });
     } else {
       next();
     }
-  } catch (error: any) {
-    const { response } = error;
+  } catch (error: unknown) {
+    let errorCode: string;
+    let errorMessage: string;
 
-    next(response.data.error);
+    if (axios.isAxiosError(error)) {
+      if (error.response?.data.error) {
+        const scanError = error.response?.data.error;
+        errorCode = scanError.code;
+        errorMessage = scanError.message;
+      } else {
+        errorCode = error.code as ErrorCode;
+        errorMessage = error.message;
+      }
+    } else {
+      errorCode = ErrorCode.UnknownError;
+      errorMessage = 'Server error.';
+    }
+
+    next(new CustomError(errorCode as ErrorCode, errorMessage));
   }
 };
 
