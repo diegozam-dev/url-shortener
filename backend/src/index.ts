c3400cdeca8d9c0484eff9b85bd409750ce8b916
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-04 15:38:48 -0500

    move cors to post endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import express, { json } from 'express';
-import cors from 'cors';
 import router from './routes/index';
 import { PORT } from './config';
 import routeNotFound from './middlewares/routeNotFound.middleware';
@@ -7,14 +6,8 @@ import errorHandler from './middlewares/errorHandler.middleware';
 
 const app = express();
 
-const corsOptions = {
-  origin: 'https://url-shortener-pearl-eta.vercel.app/',
-  methods: 'GET,POST'
-};
-
 // Config
 app.use(json());
-app.use(cors(corsOptions));
 app.disable('x-powered-by');
 
 // Routes
--- backend/src/routes/shortUrl.routes.ts
+++ backend/src/routes/shortUrl.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import cors from 'cors';
 import ShortUrlController from '../controllers/shortUrl.controller';
 import scanUrl from '../middlewares/scanUrl.middleware';
 import validateUrl from '../middlewares/validateUrl.middleware';
@@ -7,8 +8,14 @@ import rateLimiter from '../middlewares/rateLimiter.middleware';
 const shortUrlController = new ShortUrlController();
 const shortUrlRouter = Router();
 
+const corsOptions = {
+  origin: 'https://url-shortener-pearl-eta.vercel.app/',
+  methods: 'GET,POST'
+};
+
 shortUrlRouter.post(
   '/encode',
+  cors(corsOptions),
   rateLimiter,
   validateUrl,
   scanUrl,
