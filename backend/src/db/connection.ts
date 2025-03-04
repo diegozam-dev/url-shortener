54f0ee06cb7c8762fb128c18fcb640bc31b3dc21
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-04 14:18:12 -0500

    add database url and token

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@libsql/client';
-// import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from '../config';
+import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from '../config';
 
 export const db = createClient({
-  url: 'http://127.0.0.1:8080'
-  // authToken: TURSO_AUTH_TOKEN
+  url: TURSO_DATABASE_URL as string,
+  authToken: TURSO_AUTH_TOKEN
 });
