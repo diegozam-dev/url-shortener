9aa9c458741c03a309ac02cf8c6a70bf1da3bd07
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-04 14:17:42 -0500

    remove local db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import express, { json } from 'express';
 import cors from 'cors';
 import router from './routes/index';
 import { PORT } from './config';
-import { db } from './db/connection';
 import routeNotFound from './middlewares/routeNotFound.middleware';
 import errorHandler from './middlewares/errorHandler.middleware';
 
@@ -19,16 +18,6 @@ app.use(router);
 // Middlewares
 router.use(routeNotFound);
 router.use(errorHandler);
-
-(async () => {
-  await db.batch(
-    [
-      'DROP TABLE IF EXISTS short_urls',
-      'CREATE TABLE IF NOT EXISTS short_urls (id INTEGER PRIMARY KEY AUTOINCREMENT, original_url TEXT NOT NULL UNIQUE, short_url TEXT UNIQUE)'
-    ],
-    'write'
-  );
-})();
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
