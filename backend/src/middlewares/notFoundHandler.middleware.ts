a89a6d5ae823aee713e447c8bc6972c101836d42
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-03-05 16:56:36 -0500

    change response of notFoundHandler

--- backend/src/middlewares/notFoundHandler.middleware.ts
+++ backend/src/middlewares/notFoundHandler.middleware.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
-import { NOT_FOUND_PAGE_URL } from '../config';
+// import { NOT_FOUND_PAGE_URL } from '../config';
+import { ErrorCode } from '../types/error';
 
-const routeNotFound = (_req: Request, res: Response) => {
-  res.redirect(301, NOT_FOUND_PAGE_URL as string);
+const notFoundHandler = (_req: Request, res: Response) => {
+  // res.redirect(301, NOT_FOUND_PAGE_URL as string);
+  res.status(404).json({ response: ErrorCode.NotFound, message: 'Not found.' });
 };
 
-export default routeNotFound;
+export default notFoundHandler;
